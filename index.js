e03576d52ca1019be58c91db818e8447dd04dfdb
Braeden Gendron <brjgendron@gmail.com>
2019-06-13 10:07:06 -0700

    big fucing update, added signup/login and removed the random username and color generation on each visit. still broken as fuck and crashes a lot but i'm pushing the changes anyway lmao

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let http    = require("http"),
 		bodyparser = require("body-parser"),
 		{ check, validationResult } = require("express-validator/check"),
 		// sanitizeBody = require("express-validator/filter"),
-		bcrypt = require("bcrypt");
+		bcrypt = require("bcrypt"),
+		cookieParser = require("cookie-parser")
+		user = require("./user");
 
 const pool = mysql.createPool({
 	connectionLimit: 100,
@@ -22,74 +24,202 @@ let io = socket(server);
 const port = process.env.PORT || 3000;
 let hostname = "0.0.0.0";
 
+app.set("view engine", "pug");
+app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyparser.urlencoded({extended:true}));
 
+function usernameExists(username_, callback) {
+	pool.query("SELECT username FROM users WHERE username = ?", username_, (err, res) => {
+		if (err) {
+			callback(err, null);
+		} else {
+			if (res.length !== 0) {
+				callback(null, true);
+			} else {
+				callback(null, false);
+			}
+		}
+	});
+}
+
+function checkCookie(username, hash, callback) {
+	bcrypt.compare(username, hash, (err, same) => {
+		if (err) callback(err, null);
+		
+		if (same) {
+			callback(null, true);
+		} else {
+			callback(null, false);
+		}
+	});
+}
+
+function signup(username_, password_, callback) {
+	const query = "INSERT INTO users SET ?";
+
+	usernameExists(username_, (err, exists) => {
+		if (exists) { // check if username is already taken
+			callback(null, true);
+		} else { // if not, hash password and add user to database
+			callback(null, false);
+			bcrypt.hash(password_, 16, (err, hash) => {
+				pool.query(query, { id: null, username: username_, password: hash, color: genColorHsl(50, 80, 90), globalAdmin: (username_ === "bones_mcstoner" || username_ === "BigCal") ? true : false }, (err, res) => {
+					if (err) throw err;
+					console.log(res);
+				});
+			});
+		}
+	});
+}
+
+function login(username_, password_, callback) {
+	let query = "SELECT password FROM users WHERE username = ?";
+	pool.query(query, username_, (err, rows) => {
+		if (err) throw err;
+
+		if (rows.length !== 0) {
+			console.log(rows);
+			bcrypt.compare(password_, rows[0].password, (err, same) => {
+				if (err) callback(err, null);
+
+				callback(null, same);
+			});
+		}
+	});
+}
+
 
 app.get("/", (req, res) => {
-	res.sendFile(`${__dirname}/public/html/index/index.min.html`);
+	res.redirect("/chat");
+});
+
+app.get("/chat", (req, res) => {
+	io.on("connection", (socket) => {
+		let connectedUsers = [];
+		pool.query("SELECT * FROM messages", (err, rows) => {
+			if (rows.length > 0) {
+				if (rows.length >= 50) {
+					let offset = rows.length - 50;
+					console.log("rows.length:", rows.length, "\noffset:", offset);
+
+					for (var i = offset; i < rows.length; i++) {
+						console.log(i);
+						io.to(socket.id).emit("show previous messages", rows[i]);
+					}
+				} else {
+					for (var i = 0; i < rows.length; i++) {
+						io.to(socket.id).emit("show previous messages", rows[i]);
+					}
+				}
+			}
+		});
+
+		let query = "SELECT id, username, color, globalAdmin FROM users WHERE username = ?";
+		pool.query(query, req.cookies.user.x, (err, rows) => {
+			if (err) throw err;
+
+			let userSet = {
+				id: rows[0].id,
+				username: rows[0].username,
+				color: rows[0].color,
+				globalAdmin: rows[0].globalAdmin
+			};
+
+			connectedUsers.push(userSet);
+
+			console.log(userSet);
+			io.emit("new connection", userSet, connectedUsers);
+
+			socket.on("message", (message_) => {
+				let messageSet = {
+					id: null,
+					contents: message_,
+					senderID: userSet.id,
+					senderUsername: userSet.username,
+					senderUsernameWithID: `${userSet.username}-${userSet.id}`,
+					senderColorHSL: userSet.color,
+					messageTimestamp: null,
+					server: "temp-server",
+					channel: "temp-channel"
+				};
+				
+				let query = "INSERT INTO ?? SET ?";
+				let insert = ["messages", messageSet];
+				pool.query(query, insert, (err, res) => {
+					if (err) throw err;
+
+					pool.query("SELECT * FROM messages WHERE id = ?", res.insertId, (err, rows) => {
+						if (err) throw err;
+		
+						console.log(rows[0]);
+						io.emit("message", rows[0]);
+					});
+				});
+			});
+		});
+	});
+
+	if (Object.keys(req.cookies).includes("user", 0)) {
+
+		checkCookie(req.cookies.user.x, req.cookies.user.y, (err, valid) => {
+			if (valid) {
+				res.sendFile(`${__dirname}/public/html/index/index.min.html`);
+			} else {
+				res.redirect("/login");
+			}
+		});
+	} else {
+		res.redirect("/login");
+	}
 });
 
 app.get("/signup", (req, res) => {
 	res.sendFile(`${__dirname}/public/html/signup/signup.html`);
 });
 
-app.post("/processform", [check("usernamefield").isLength({ min: 5, max: 25 }), check("passwordfield").isLength({ min: 8, max: 128 })], (req, res) => {
+app.get("/login", (req, res) => {
+	res.sendFile(`${__dirname}/public/html/login/login.html`);
+});
 
+app.post("/processlogin", [check("usernamefield").isLength({ min: 5, max: 25 }), check("passwordfield").isLength({ min: 8, max: 128 })], (req, res) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-	let UserSet = {
-		id: null,
-		username: req.body.usernamefield,
-		password: req.body.passwordfield
-	}
-
-	const saltRounds = 16;
-	// const username = req.body.user_name;
-	// const plainTextPassword = req.body.pass_word;
-
-	bcrypt.hash(UserSet.password, saltRounds, (err, hash) => {
-		if (err) throw err;
-
-		UserSet.password = hash;
-		let inserts = ["users", UserSet];
-
-		pool.query("SELECT username FROM users", (err, rows) => {
-			let exists = false;
-			for (var i = 0; i < rows.length; i++) {
-				if (UserSet.username == rows[i].username) {
-					exists = true;
-					// return;
-				} else {
-					exists = false;
-				}
-
-			}
+	login(req.body.usernamefield, req.body.passwordfield, (err, success) => {
+		if (success) {
+			bcrypt.hash(req.body.usernamefield, 10, (err, hash) => {
+				if (err) throw err;
+				res.cookie("user", { x: req.body.usernamefield, y: hash });
+				res.redirect("/");
+			});
 			
-			console.log(exists);
-			if (!exists) {
-				pool.query("INSERT INTO ?? SET ?", inserts, (err, res) => {
-					if (err) throw err;
-					console.log(res);
-				});
-			} else {
-				console.log("fuck you bitch");
-			}
-		});
-
-
-		// bcrypt.compare(plainTextPassword, hash, (err, res) => {
-		// 	console.log(res);
-		// });
+		} else {
+			res.send("username and password do not match");
+		}
 	});
-
-	res.send("i need a message here");
 });
 
+app.post("/processsignup", [check("usernamefield").isLength({ min: 5, max: 25 }), check("passwordfield").isLength({ min: 8, max: 128 })], (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-// app.get("/login", (req, res) => {
-// });
+	signup(req.body.usernamefield, req.body.passwordfield, (err, accountExists) => {
+		if (accountExists) {
+			res.send("invalid username or password");
+		} else {
+			bcrypt.hash(req.body.usernamefield, 10, (err, hash) => {
+				if (err) throw err;
+				res.cookie("user", { x: req.body.usernamefield, y: hash });
+				res.redirect("/");
+			});
+		}
+	});
+});
+
+app.get("/usersettings", (req, res) => {
+	res.render("usersettings.pug", { title: "User Settings", placeholder: req.cookies.user.x, color: "red" });
+});
 
 function genUserID(length) {
 	let result = "";
@@ -104,8 +234,8 @@ function genUserID(length) {
 }
 
 function genColorHsl(minL, maxL, saturation) {
-	let color = `hsl(${Math.floor(Math.random() * 360)}, ${saturation}%, ${Math.floor(Math.random() * (maxL - minL)) + minL}%)`;
-	return color;
+	// let color = `hsl(${Math.floor(Math.random() * 360)}, ${saturation}%, ${Math.floor(Math.random() * (maxL - minL)) + minL}%)`;
+	return `hsl(${Math.floor(Math.random() * 360)}, ${saturation}%, ${Math.floor(Math.random() * (maxL - minL)) + minL}%)`;
 }
 
 function genTimestamp() {
@@ -135,134 +265,129 @@ class Message {
 
 let users = [];
 
-io.on("connection", (socket_) => {
-	pool.query("SELECT * FROM messages", (err, rows) => {
-		if (err) throw err;
+function connect(cookie, callback) {
+	callback();
+}
+
+// function getCookies(callback) {
+	
+// }
+
+// io.on("connection", (socket_) => {
+// 	pool.query("SELECT * FROM messages", (err, rows) => {
+// 		if (err) throw err;
+
+// 		if (rows.length > 0) {
+// 			if (rows.length >= 50) {
+// 				let offset = rows.length - 50;
+// 				console.log("rows.length:", rows.length, "\noffset:", offset);
 		
+// 				for (var i = offset; i < rows.length; i++) {
+// 					console.log(i);
+// 					io.to(socket_.id).emit("show previous messages", rows[i]);
+// 				}
+// 			} else {
+// 				for (var i = 0; i < rows.length; i++) {
+// 					io.to(socket_.id).emit("show previous messages", rows[i]);
+// 				}
+// 			}
+// 		}
+// 	});
 
-		if (rows.length > 0) {
-			if (rows.length >= 50) {
-				let offset = rows.length - 50;
-				console.log("rows.length:", rows.length, "\noffset:", offset);
+// 	let user = new User(genUserID(6), "", genColorHsl(50, 80, 90));
+// 	users.push(user);
+
+// 	io.emit("user.connect", users, user);
+// 	console.log(`\nuser ${users.slice(-1)[0].username} connected`);
+// 	console.log("currently connected users:", users); 
+// 	console.log(io.engine.clientsCount);
+
+
+// 	socket_.on("user.setusername", (newUsername_, oldUsername_, user_) => {
+// 		oldUsername_ = user.username;
+// 		user.username = newUsername_;
+// 		console.log(user.id, "has changed their username to", newUsername_);
+// 		io.emit("user.setusername", users, user, oldUsername_);
+// 	});
+
+// 	socket_.on("chat.message", (message_) => {
 		
-				for (var i = offset; i < rows.length; i++) {
-					console.log(i);
-					io.to(socket_.id).emit("garbage test", rows[i]);
-				}
-			} else {
-				for (var i = 0; i < rows.length; i++) {
-					io.to(socket_.id).emit("garbage test", rows[i]);
-				}
-			}
-		}
-	});
+// 		timestamp = genTimestamp();
+// 		let message = new Message(message_, user, timestamp);
 
-	let user = new User(genUserID(6), "", genColorHsl(50, 80, 90));
-	users.push(user);
+// 		let messageQuery = {
+// 			id: null,
+// 			contents: message.message,
+// 			senderID: message.sender.id,
+// 			senderUsername: message.sender.username,
+// 			senderColorHSL: message.sender.color,
+// 			messageTimestamp: null,
+// 			server: "temp-server",
+// 			channel: "temp-channel"
+// 		};
 
-	io.emit("user.connect", users, user);
-	console.log(`\nuser ${users.slice(-1)[0].username} connected`);
-	console.log("currently connected users:", users); 
-	console.log(io.engine.clientsCount);
+// 		let table = "messages";
+// 		let inserts = ["messages", messageQuery];
+// 		pool.query("INSERT INTO ?? SET ?", inserts, (err, res) => {
+// 			if (err) throw err;
+// 			pool.query("SELECT * FROM messages WHERE id = ?", res.insertId, (err, rows) => {
+// 				if (err) throw err;
 
+// 				io.emit("chat.message", rows[0]);
+// 				console.log(rows[0]);
+// 			});
+// 		});
 
-	socket_.on("user.setusername", (newUsername_, oldUsername_, user_) => {
-		oldUsername_ = user.username;
-		user.username = newUsername_;
-		console.log(user.id, "has changed their username to", newUsername_);
-		io.emit("user.setusername", users, user, oldUsername_);
-	});
+// 		// checkCookie(req.cookies.user.x, req.cookies.user.y, (err, valid) => {
+// 		// 	if (err) throw err;
 
-	socket_.on("chat.message", (message_) => {
-		
-		timestamp = genTimestamp();
-		let message = new Message(message_, user, timestamp);
+// 		// 	let messageSet = {
+// 		// 		id: null,
+// 		// 		contents: message_,
+// 		// 		sender
+// 		// 	}
+// 		// 	let query = "INSERT INTO ?? SET ?";
+// 		// 	let inserts = ["messages", messageQuery];
+// 		// 	if (valid) {
+// 		// 		pool.query(query, inserts, (err, res) => {
+// 		// 			if (err) throw err;
 
-		let messageQuery = {
-			id: null,
-			contents: message.message,
-			senderID: message.sender.id,
-			senderUsername: message.sender.username,
-			senderColorHSL: message.sender.color,
-			messageTimestamp: null,
-			server: "temp-server",
-			channel: "temp-channel"
-		};
+// 		// 			pool.query()
 
-		let table = "messages";
-		let inserts = ["messages", messageQuery];
-		pool.query("INSERT INTO ?? SET ?", inserts, (err, res) => {
-			if (err) throw err;
-			pool.query("SELECT * FROM messages WHERE id = ?", res.insertId, (err, rows) => {
-				if (err) throw err;
-
-				io.emit("chat.message", rows[0]);
-				console.log(rows[0]);
-			});
-		});
-
-		// io.emit("chat.message", message);
-		console.log(`\n[${message.timestamp}] ${message.sender.username}: ${message.message}`);
-	});
+// 		// 			io.emit("chat.message", rows[0]);
+// 		// 		});
+// 		// 	}
+// 		// });
+// 		console.log(`\n[${message.timestamp}] ${message.sender.username}: ${message.message}`);
+// 	});
 
 
-	socket_.on("disconnect", () => {
-		let lastUser;
+// 	socket_.on("disconnect", () => {
+// 		let lastUser;
 
-		for (var i = 0; i < users.length; i++) {
-			if (user == users[i]) {
-				lastUser = users[i];
-				users.splice(i, 1);
-			}
-		}
+// 		for (var i = 0; i < users.length; i++) {
+// 			if (user == users[i]) {
+// 				lastUser = users[i];
+// 				users.splice(i, 1);
+// 			}
+// 		}
 
-		io.emit("user.disconnect", users, user);
-		console.log(`\nuser ${lastUser.username} (${lastUser.id}) disconnected`);
-		console.log("currently connected users:", users); 
-	});
+// 		io.emit("user.disconnect", users, user);
+// 		console.log(`\nuser ${lastUser.username} (${lastUser.id}) disconnected`);
+// 		console.log("currently connected users:", users); 
+// 	});
 
-	socket_.on("deeznuts", (deeznuts_) => {
-		deeznuts_ = "deeznuts.ogg";
-		io.emit("deeznuts", deeznuts_);
-	}); 
+// 	socket_.on("deeznuts", (deeznuts_) => {
+// 		deeznuts_ = "deeznuts.ogg";
+// 		io.emit("deeznuts", deeznuts_);
+// 	}); 
 
-	socket_.on("username request", (username_) => {
-		let querySet = ["users"];
-		var match = false;
-		pool.query("SELECT username FROM ??", querySet, (err, rows) => {
-			if (err) throw err;
-
-			for (var i = 0; i < rows.length; i++) {
-				console.log(rows[i]);
-				if (username_ == rows[i].username) {
-					match = true;
-					io.emit("matched username", match);
-					console.log(match);
-				} else {
-					match = false;
-					io.emit("matched username", match);
-					console.log(match);
-				}
-			}
-
-			// if (rows[0].username == username_) {
-			// 	match = true;
-			// 	socket_.emit("matched username", match);
-			// 	console.log("usernames match");
-			// } else {
-			// 	match = false;
-			// 	socket_.emit("matched username", match);
-			// 	console.log("usernames do not match");
-			// }
-		});
-		// let inserts = ["users", "username", username_];
-		// pool.query("SELECT * FROM ?? WHERE ? = ?", inserts, (err, rows) => {
-		// 	console.log(rows);
-		// });
-		
-		// console.log(username_);
-	});
-});
+// 	socket_.on("username request", (username_) => {
+// 		usernameExists(username_, (err, res) => {
+// 			io.emit("matched username", res);
+// 		});
+// 	});
+// });
 
 server.listen(port, hostname, () => {
 	console.log(`listening on port ${port}`);
--- public/js/events.js
+++ public/js/events.js
@@ -5,6 +5,11 @@
 	const usernameCharLimit = 25;
 	
 	let messages = document.querySelector("#messages");
+
+	let cookieJson = JSON.parse(decodeURIComponent(document.cookie).split("user=j:", -1)[1]);
+	// console.log(cookieJson.x);
+
+
 	let usersList = document.querySelector("#users");
 
 	let chatbox = document.querySelector("#chatbox");
@@ -73,6 +78,44 @@
 		return messageJSON.container.html;
 	}
 
+
+	function genListObj2(sender_) {
+		let messageJSON = {
+			container: {
+				html: document.createElement("li"),
+				class: "message"
+			}
+		};
+
+		for (var i = 1; i < arguments.length; i++) {
+			messageJSON[`domObj${i}`] = {
+				html: document.createElement(Object.values(arguments[i])[0]),
+				class: Object.values(arguments[i])[1],
+				contents: Object.values(arguments[i])[2]
+			}
+		}
+
+		const values = Object.values(messageJSON);
+		for (var x = 0; x < values.length; x++) {
+			if (x < values.length - 1) {
+				let y = x;
+				values[0].html.appendChild(values.slice(1)[y].html)
+			}
+
+			if (values[x].class !== "") {
+				values[x].html.classList.add(values[x].class);
+				
+				if (values[x].class === sender_.id) {
+					values[x].html.setAttribute("style", `color: ${sender_.color}; font-weight: 500;`);
+				}
+			}
+
+			if (values[x].contents != undefined) values[x].html.appendChild(document.createTextNode(values[x].contents));
+		}
+
+		return messageJSON.container.html;
+	}
+
 	function fillList(list_, items_) {
 		let listItem;
 
@@ -92,47 +135,75 @@
 	document.querySelector("#compose").addEventListener("submit", (event_) => {
 		event_.preventDefault();
 		if (chatbox.value.length > 0 && chatbox.value.length <= messageCharLimit) {
-			socket.emit("chat.message", chatbox.value);
+			socket.emit("message", chatbox.value);
 			chatbox.value = "";
 		} else if (chatbox.value.length > messageCharLimit) {
 			alert(`Your message exceeds the character limit of ${messageCharLimit}`);
 		}
 	});
 
-	socket.on("user.connect", (users_, user_) => {
-		document.querySelector("#change-username").onclick = (event_) => {
-			event_.preventDefault();
-			let newUsername = prompt("Enter a new username");
-			if (newUsername.length > 0 && newUsername.length <= 25) {
-				socket.emit("user.setusername", newUsername, user_);
-			} else if (newUsername.length > usernameCharLimit) {
-				alert("Your username exceeds the character limit of " + usernameCharLimit);
-			}
-		};
-
-		fillList(usersList, users_);
-		
-		messages.appendChild(genListObj(user_, {html: "span", class: "", contents: "User "}, {html: "span", class: user_.id, contents: user_.id}, {html: "span", class: "", contents: " has connected"}));
-		console.log(`user ${user_.username} has connected`);
+	socket.on("new connection", (userData_, connectedUsers_) => {
+		fillList(usersList, connectedUsers_);
+		messages.appendChild(
+			genListObj(
+				userData_,
+				{ html: "span", class: "", contents: "User " },
+				{ html: "span", class: userData_.id, contents: `${userData_.username}-${userData_.id}` },
+				{ html: "span", class: "", contents: " has connected"}
+			)
+		);
 	});
 
-	socket.on("user.setusername", (users_, user_, oldUsername_) => {
-		if (user_.id != oldUsername_) {
-			messages.appendChild(genListObj(user_, {html: "span", class: "", contents: "User "}, {html: "span", class: user_.id, contents: `${oldUsername_} (${user_.id})`}, {html: "span", class: "", contents: " has changed their username to "}, {html: "span", class: user_.id, contents: user_.username}));
-		} else {
-			messages.appendChild(genListObj(user_, {html: "span", class: "", contents: "User "}, {html: "span", class: user_.id, contents: user_.id}, {html: "span", class: "", contents: " has changed their username to "}, {html: "span", class: user_.id, contents: user_.username}));
-		}
-		fillList(usersList, users_);
-	});
+	socket.on("message", (message_) => {
+		messages.appendChild(
+			genListObj(
+				{ id: message_.senderID, username: message_.senderUsername, color: message_.senderColorHSL },
+				{ html: "span", class: "message-timestamp", contents: `[${message_.messageTimestamp}] ` },
+				{ html: "span", class: message_.senderID, contents: message_.senderUsernameWithID },
+				{ html: "span", class: "message-text", contents: `: ${message_.contents}` }
+			)
+		);
 
-
-	socket.on("chat.message", (message_) => {
-		messages.appendChild(genListObj({id: message_.senderID, username: message_.senderUsername, color: message_.senderColorHSL}, {html: "span", class: "message-timestamp", contents: `[${message_.messageTimestamp}] `}, {html: "span", class: message_.senderID, contents: message_.senderUsername}, {html: "span", class: "message-text", contents: `: ${message_.contents}`}));
-		
-		document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollTop = document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollHeight;
+		document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollTop =
+		document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollHeight;
 
 		console.log(message_);
 	});
+
+	// socket.on("user.connect", (users_, user_) => {
+		
+	// 	document.querySelector("#change-username").onclick = (event_) => {
+	// 		event_.preventDefault();
+	// 		let newUsername = prompt("Enter a new username");
+	// 		if (newUsername.length > 0 && newUsername.length <= 25) {
+	// 			socket.emit("user.setusername", newUsername, user_);
+	// 		} else if (newUsername.length > usernameCharLimit) {
+	// 			alert("Your username exceeds the character limit of " + usernameCharLimit);
+	// 		}
+	// 	};
+
+	// 	fillList(usersList, users_);
+	// 	messages.appendChild(genListObj(user_, {html: "span", class: "", contents: "User "}, {html: "span", class: user_.id, contents: user_.id}, {html: "span", class: "", contents: " has connected"}));
+	// 	console.log(`user ${user_.username} has connected`);
+	// });
+
+	// socket.on("user.setusername", (users_, user_, oldUsername_) => {
+	// 	if (user_.id != oldUsername_) {
+	// 		messages.appendChild(genListObj(user_, {html: "span", class: "", contents: "User "}, {html: "span", class: user_.id, contents: `${oldUsername_} (${user_.id})`}, {html: "span", class: "", contents: " has changed their username to "}, {html: "span", class: user_.id, contents: user_.username}));
+	// 	} else {
+	// 		messages.appendChild(genListObj(user_, {html: "span", class: "", contents: "User "}, {html: "span", class: user_.id, contents: user_.id}, {html: "span", class: "", contents: " has changed their username to "}, {html: "span", class: user_.id, contents: user_.username}));
+	// 	}
+	// 	fillList(usersList, users_);
+	// });
+
+
+	// socket.on("chat.message", (message_) => {
+	// 	messages.appendChild(genListObj({id: message_.senderID, username: message_.senderUsername, color: message_.senderColorHSL}, {html: "span", class: "message-timestamp", contents: `[${message_.messageTimestamp}] `}, {html: "span", class: message_.senderID, contents: message_.senderUsername}, {html: "span", class: "message-text", contents: `: ${message_.contents}`}));
+		
+	// 	document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollTop = document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollHeight;
+
+	// 	console.log(message_);
+	// });
 	// socket.on("chat.message", (message_) => {
 	// 	messages.appendChild(genListObj(message_.sender, {html: "span", class: "message-timestamp", contents: `[${message_.timestamp}] `}, {html: "span", class: message_.sender.id, contents: message_.sender.username}, {html: "span", class: "message-text", contents: `: ${message_.message}`}));
 		
@@ -143,11 +214,18 @@
 	// 	console.log(`[${message_.timestamp}] ${message_.sender.username}: ${message_.message}`);
 	// });
 
-	socket.on("garbage test", (data_) => {
-		
-		console.log(data_.senderID);
-		messages.appendChild(genListObj({id: data_.senderID, username: data_.senderUsername, color: data_.senderColorHSL}, {html: "span", class: "message-timestamp", contents: `[${data_.messageTimestamp}] `}, {html: "span", class: data_.senderID, contents: data_.senderUsername}, {html: "span", class: "message-text", contents: `: ${data_.contents}`}));
-		document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollTop = document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollHeight;
+	socket.on("show previous messages", (data_) => {
+		messages.appendChild(
+			genListObj(
+				{ id: data_.senderID, username: data_.senderUsername, color: data_.senderColorHSL },
+				{ html: "span", class: "message-timestamp", contents: `[${data_.messageTimestamp}] ` },
+				{ html: "span", class: data_.senderID, contents: data_.senderUsernameWithID },
+				{ html: "span", class: "message-text", contents: `: ${data_.contents}`}
+			)
+		);
+
+		document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollTop =
+		document.querySelector("#messages-container").SimpleBar.getScrollElement().scrollHeight;
 	});
 
 	socket.on("user.disconnect", (users_, user_) => {
